9c7fe32d35aaed3c6ae372948f48885db4637d46
agustin.cantero <agustin.cantero@softtek.com>
2020-05-16 22:07:14 -0300

    practica routing

---
+++ curso/routing-start/src/app/auth-guard.service.ts
@@ -0,0 +1,30 @@
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivateChild } from "@angular/router";
+import { Injectable } from "@angular/core";
+import { AuthService } from "./auth.service";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AuthGuard implements CanActivate, CanActivateChild {
+
+    constructor(private authService: AuthService, private router: Router) { }
+
+    canActivate(route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+        return this.authService.isAuthenticated()
+            .then(
+                (authenticated: boolean) => {
+                    if (authenticated) {
+                        return true;
+                    } else {
+                        this.router.navigate(['/']);
+                    }
+                }
+            );
+    }
+
+    canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | import("@angular/router").UrlTree | Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
+        return this.canActivate(childRoute, state);
+    }
+
+
+}
