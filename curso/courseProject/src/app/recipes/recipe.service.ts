90b053aadcc23968d51fd0e14085e9e78f1b7700
agustin.cantero <agustin.cantero@softtek.com>
2020-05-19 11:13:12 -0300

    avance del curso

--- curso/courseProject/src/app/recipes/recipe.service.ts
+++ curso/courseProject/src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable, Output } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
@@ -8,8 +8,6 @@ import { Recipe } from './recipe.model';
   providedIn: 'root'
 })
 export class RecipeService {
-
-  @Output() recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe("Test Recipe", "This is a sample test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg", [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)])
